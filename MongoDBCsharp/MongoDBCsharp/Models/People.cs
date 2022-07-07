0330e0399c0a3285d7ca9a0fb9ef4d295edca8c5
Widinson Danilo <widinsondanilo@gmail.com>
2022-07-07 17:16:51 -0500

    c# person

---
+++ MongoDBCsharp/MongoDBCsharp/Models/People.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDBCsharp.Models
+{
+   public class People
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("name")]
+        public string name { get; set; }
+
+        [BsonElement("age")]
+        public int age { get; set; }
+
+
+        [BsonElement("country")]
+        public string country { get; set; }
+    }
+}
